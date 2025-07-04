0abb990606117d639170032bd0e9842eae1585f4
DanYT2 <danwwaititu@gmail.com>
2025-07-04 14:14:31 +0300

    Enhance main.go to implement a basic HTTP server that responds with "Hello, World!"

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	server := &http.Server{
+		Addr: ":3000",
+		Handler : http.HandlerFunc(basicHandler),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Server is running on port 3000")
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
 }
